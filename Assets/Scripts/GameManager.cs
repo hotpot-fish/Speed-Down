bd36a4e9d6fc94ee5ba9d6aac56b7b0746338d08
hotpotfish <732028448@qq.com>
2022-03-04 12:11:55 +0800

    [Add] assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+    public GameObject gameOverPanel;
+    public Text timeScore;
+
+
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        timeScore.text = Time.timeSinceLevelLoad.ToString("00");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void GameOver(bool dead)
+    {
+        if (dead)
+        {
+            instance.gameOverPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> platforms = new List<GameObject>();
+    
+    public float spawnTime;
+    private float countTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SpawnPlatform()
+    {
+        Vector3 spawnPosition = transform.position;
+        spawnPosition.x = Random.Range(-3.0f, 3.0f);
+        int index = Random.Range(0, platforms.Count);
+        GameObject go = Instantiate(platforms[index], spawnPosition,
+       Quaternion.identity);
+        go.transform.SetParent(this.gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime;
+
+        if (countTime >= spawnTime)
+        {
+            SpawnPlatform();
+            countTime = 0;
+        }
+    }
+}
